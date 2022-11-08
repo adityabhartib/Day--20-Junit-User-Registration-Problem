945c01f1789186be296e53585da08e2465b9b8ec
adityabhartib <adityabharti.ab@gmail.com>
2022-11-08 20:02:05 +0530

    Added : Welcome msg

---
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserRegistrationTest {
+@Test
+    void given_first_name_should_returnTrue() {
+        UserValidation validation = new UserValidation();
+        Boolean result=validation.checkFirstName("Aditya");
+        Assertions.assertEquals(result,true);
+    }
+}
