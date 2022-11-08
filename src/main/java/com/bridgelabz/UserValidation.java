6201563e89cd81e599fbee66e8c0814c8b879301
adityabhartib <adityabharti.ab@gmail.com>
2022-11-08 22:32:31 +0530

    Added :UC4 validate Phone Number

--- src/main/java/com/bridgelabz/UserValidation.java
+++ src/main/java/com/bridgelabz/UserValidation.java
@@ -4,6 +4,7 @@ import java.util.regex.Pattern;
 public class UserValidation {
     public static final String VALIDATE_NAME="^[A-z]+[a-z]{2,}$";
     public static final String EMAIL_ID="^[A-Za-z]{3,}[.]?[A-Za-z]{2,}[@]{1}[a-z]{2,}[.]{1}[a-z]{2,}$";
+    public static final String PHONE_NUMBER="^[0-9]+\s[0-9]{10}$";
     public Boolean checkFirstName(String firstName) {
         return (Pattern.matches(VALIDATE_NAME, firstName));
     }
@@ -14,5 +15,9 @@ public class UserValidation {
 
     public Boolean checkemailId(String emailId) {
         return (Pattern.matches(EMAIL_ID,emailId));
+    }
+
+    public Boolean phoneNumber(String phoneNumber) {
+        return (Pattern.matches(PHONE_NUMBER,phoneNumber));
     }
 }
